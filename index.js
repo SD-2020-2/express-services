839e20e62fec61400aa976a6a076f39abd581060
augusticor <rojascruzoscar@gmail.com>
2020-11-13 18:52:25 -0500

    Iniciando el primer repositorio para sistemas distribuidos
    - Se crea el archivo index.js para entrada del programa
    - Index.js contiene la creacion del servidor así como la creacion de una
    lista de objetos tipo json para trabajar
    - Se agregan las dependencias express y nodemon

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/*
+Authors:
+Linda Matilde Rodriguez -
+Cristhian Chamorro - 
+Oscar Augusto Rojas Cruz - 201521379
+*/
+const express = require('express');
+const server = express();
+const port = 3000;
+
+const bodyParser = require('body-parser');
+
+server.use(bodyParser.json());
+
+let users = [
+	{ name: 'Linda', code: 135 },
+	{ name: 'Cristhian', code: 387 },
+	{ name: 'Oscar', code: 379 },
+	{ name: 'Ana', code: 321 },
+	{ name: 'Juan', code: 123 },
+	{ name: 'Alex', code: 345 },
+	{ name: 'Jhon', code: 683 },
+	{ name: 'Maria', code: 547 },
+	{ name: 'Danna', code: 782 },
+	{ name: 'Alejandra', code: 431 },
+];
+
+server.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+server.get('/users', (req, res) => {
+	res.send(users);
+});
+
+server.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
